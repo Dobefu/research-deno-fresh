97090077544974940e017c1f77bb53046d8ae896
Connor van Spronssen <info@dobefu.com>
2024-10-01 17:19:40 +0200

    Add some shadows

--- components/LayoutElements/Header.tsx
+++ components/LayoutElements/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header class="p-4">
-      <a href="/" class="text-lg flex flex-col items-center gap-4 font-medium">
+    <header class="p-4 py-2 shadow-md">
+      <a href="/" class="text-lg flex flex-col items-center gap-2 font-medium">
         <img
           class="w-12 h-12"
           src="/logo.svg"
